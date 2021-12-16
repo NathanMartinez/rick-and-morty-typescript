3eee0387f0d59c70f63834740f4df22959c99663
Nate Martinez <natemartinez@nates-mbp.abqdowns.local>
2021-12-15 18:35:44 -0700

    Removed code bloat. Added Navigation.

---
+++ src/components/Navigation.js
@@ -0,0 +1,14 @@
+function Navigation() {
+    return (
+        <nav>
+            <h1>Rick and Morty</h1>
+            <ul>
+                <li>Characters</li>
+                <li>Episodes</li>
+                <li>Locations</li>
+            </ul>
+        </nav>
+    )
+}
+
+export default Navigation
